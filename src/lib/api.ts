774b4c4ec2b27dda3a4967293ba424a899ba21bf
Ilham Khalik <believerrr05@gmail.com>
2024-08-22 17:54:22 +0700

    feat/products: Get products service

---
+++ src/lib/api.ts
@@ -0,0 +1,29 @@
+import type { Product } from './type';
+
+const API_BASE_URL = process.env.NEXT_PUBLIC_APIBASEURL;
+
+export const getProducts = async (
+  params?: string,
+): Promise<Product[] | null> => {
+  try {
+    const res = await fetch(`${API_BASE_URL}/products?${params}`);
+    const data = await res.json();
+    return data;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+};
+
+export const getSingleProducts = async (
+  id: number,
+): Promise<Product | null> => {
+  try {
+    const res = await fetch(`${API_BASE_URL}/products/${id}`);
+    const data = await res.json();
+    return data;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+};
