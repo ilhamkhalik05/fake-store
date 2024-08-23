ecf6d38838db7d15d818f0348d3c4a88d1e36056
Ilham Khalik <believerrr05@gmail.com>
2024-08-23 20:02:30 +0700

    ui/home: Category group badge

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Banner from '@/components/banner';
+import Category from '@/components/category';
 
 export default function Home() {
   return (
-    <div className="p-5">
+    <div className="flex flex-col gap-10 p-5">
       <Banner />
+      <Category />
     </div>
   );
 }
---
+++ src/components/category/index.tsx
@@ -0,0 +1,44 @@
+import { Badge } from '@/components/ui/badge';
+import { Diamond, Gamepad2, Refrigerator, Shirt } from 'lucide-react';
+
+export default function Category() {
+  return (
+    <div className="flex flex-row justify-center gap-3 overflow-x-auto w-full mx-3">
+      <Badge
+        variant={'outline'}
+        className="flex items-center gap-2 p-2 hover:bg-gray-100"
+      >
+        <Gamepad2 size={18} color="red" />
+        Gaming
+      </Badge>
+      <Badge
+        variant={'outline'}
+        className="flex items-center gap-2 p-2 hover:bg-gray-100"
+      >
+        <Refrigerator size={18} color="cyan" />
+        Electronic
+      </Badge>
+      <Badge
+        variant={'outline'}
+        className="flex items-center gap-2 p-2 hover:bg-gray-100"
+      >
+        <Diamond size={18} color="purple" />
+        Jewelery
+      </Badge>
+      <Badge
+        variant={'outline'}
+        className="flex items-center gap-2 p-2 hover:bg-gray-100"
+      >
+        <Shirt size={18} color="orange" />
+        Men Top
+      </Badge>
+      <Badge
+        variant={'outline'}
+        className="flex items-center gap-2 p-2 hover:bg-gray-100"
+      >
+        <Shirt size={18} color="pink" />
+        Women Top
+      </Badge>
+    </div>
+  );
+}
