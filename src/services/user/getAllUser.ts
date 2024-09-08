4a4e364b6f9f12321b12a93ec32b7b9a85ba7f02
Ilham Khalik <believerrr05@gmail.com>
2024-09-08 13:26:42 +0700

    refactor/user: Refactor get all user service

--- src/services/user/getAllUser.ts
+++ src/services/user/getAllUser.ts
@@ -4,7 +4,7 @@ import { User } from '@/lib/type';
 export const getAllUser = async (): Promise<User[] | null> => {
   try {
     const res = await fetch(`${API_BASE_URL}/users`);
-    const data = res.json();
+    const data = await res.json();
     return data;
   } catch (error) {
     console.error(error);
