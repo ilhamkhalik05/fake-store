edc68fd56b2e8c12d0dd4089166786cbca0bf843
Ilham Khalik <believerrr05@gmail.com>
2024-09-17 19:24:50 +0700

    feat/cart: Add plus product quantity service

--- src/components/products-cart-list/ProductsCartQuantity.tsx
+++ src/components/products-cart-list/ProductsCartQuantity.tsx
@@ -4,14 +4,16 @@ import { Minus, Plus } from 'lucide-react';
 
 export const ProductsCartQuantity = ({
   productQuantity,
+  plusProductQuantity,
 }: {
   productQuantity: number;
+  plusProductQuantity: () => void;
 }) => {
   return (
     <div className="flex items-center gap-3 ring-1 ring-gray-300 rounded-lg px-2 py-1">
       <Minus size={20} className="text-red-500" />
       <span>{productQuantity}</span>
-      <Plus size={20} className="text-black" />
+      <Plus size={20} className="text-black" onClick={plusProductQuantity} />
     </div>
   );
 };
--- src/hooks/useCart.ts
+++ src/hooks/useCart.ts
@@ -1,27 +1,49 @@
-import { CartSummary, ProductWithQuantity } from '@/lib/type';
+import { CartSummary, ProductsInCart } from '@/lib/type';
 import { getProductsInCart } from '@/services/cart';
 import { useEffect, useState } from 'react';
 
 export const useCart = (userId: number) => {
   const [cartSummary, setCartSummary] = useState<CartSummary>();
+  const products = cartSummary?.products;
+
+  function plusProductQuantity(productId: number) {
+    if (!productId) return;
+
+    // Add product quantity by 1
+    const updatedProducts =
+      products?.map((product) => {
+        return product.id === productId
+          ? { ...product, quantity: product.quantity + 1 }
+          : product;
+      }) || [];
+
+    // Set new state
+    setCartSummary((prevState) => {
+      if (!prevState) return prevState;
+      return {
+        ...prevState,
+        products: updatedProducts,
+      };
+    });
+  }
 
   useEffect(() => {
     async function initializeCartSummary() {
-      if (userId) {
-        const productsInCart = await getProductsInCart(userId);
+      if (!userId) return;
 
-        const newCartSummary: CartSummary = {
-          id: Math.random(),
-          products: productsInCart as ProductWithQuantity[],
-          totalPrice: 0,
-        };
+      const productsInCart = await getProductsInCart(userId);
 
-        setCartSummary(newCartSummary);
-      }
+      const initCartSummary: CartSummary = {
+        id: Math.random(),
+        products: productsInCart as ProductsInCart[],
+        totalPrice: 0,
+      };
+
+      setCartSummary(initCartSummary);
     }
 
     initializeCartSummary();
   }, []);
 
-  return { data: cartSummary };
+  return { data: cartSummary, plusProductQuantity };
 };
