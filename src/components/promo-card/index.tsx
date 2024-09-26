629abf0245447705c98836d11a0f562c57c6409c
Ilham Khalik <believerrr05@gmail.com>
2024-09-26 15:14:01 +0700

    refactor: Optimizing promo sale image

--- src/components/promo-card/index.tsx
+++ src/components/promo-card/index.tsx
@@ -1,15 +1,15 @@
-import { Card } from '../@shadcn-ui/card';
 import Image from 'next/image';
 import salePromo from '@public/sale-promo.jpg';
 
 export default function PromoCard() {
   return (
-    <Card>
-      <Image
-        className="w-full md:w-96 object-cover rounded-sm"
-        src={salePromo}
-        alt="sale"
-      />
-    </Card>
+    <Image
+      className="w-full md:w-72 object-cover rounded-sm"
+      src={salePromo}
+      alt="sale"
+      width={1000}
+      height={1000}
+      priority={true}
+    />
   );
 }
